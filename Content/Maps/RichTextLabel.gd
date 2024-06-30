9b89f0a0be2a0f37cb6c3e3d4a8a225d5360e1f8
Nickel <nsulfate@gmail.com>
2024-06-30 15:55:28 -0400

    Fixed Scene Corruption

--- Content/Maps/RichTextLabel.gd
+++ Content/Maps/RichTextLabel.gd
@@ -1,11 +1,11 @@
-#extends RichTextLabel
+extends RichTextLabel
 
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 #func _ready():
-	set_scroll_follow(true)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	get_child(0).value += 1
+	#set_scroll_follow(true)
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+	#get_child(0).value += 1
